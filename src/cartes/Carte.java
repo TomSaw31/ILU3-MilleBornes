3561d9e8b2be44dfea3faea6704c47fea842f1bd
Thomas Saurel <111185572+TomSaw31@users.noreply.github.com>
2026-03-25 22:52:53 +0100

    Refactor equals method for Carte class

--- src/cartes/Carte.java
+++ src/cartes/Carte.java
@@ -4,9 +4,6 @@ public abstract class Carte {
 	
 	@Override
 	public boolean equals(Object obj) {
-		if (obj instanceof Carte carte) {
-			return toString().equals(carte.toString());
-		}
-		return false;
+		return obj != null && this.getClass() == obj.getClass();
 	}
 }
